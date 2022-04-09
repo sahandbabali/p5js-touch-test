6604c5ce6d3171841b0c7ed095dff80d2758467c
sahandbabali <sahand.babali@gmail.com>
2022-04-09 19:13:44 +0430

    touches added

--- app.js
+++ app.js
@@ -4,8 +4,17 @@ function setup() {
 
 function draw() {
   background(200);
-  clear();
+  // clear();
   let display = touches.length + " touches";
+  if (touches.length > 0) {
+    console.log(touches);
+  }
+
+  if (touches.length > 1) {
+    for (let i = 0; i < touches.length - 1; i++) {
+      line(touches[i].x, touches[i].y, touches[i + 1].x, touches[i + 1].x);
+    }
+  }
   text(display, 5, 10);
 }
 
