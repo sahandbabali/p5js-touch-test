a7e23146c18dadc3d791fdb68adbf0dbf369b7ba
sahandbabali <sahand.babali@gmail.com>
2022-04-09 19:22:32 +0430

    touches added

--- app.js
+++ app.js
@@ -8,10 +8,11 @@ function draw() {
   let display = touches.length + " touches";
   if (touches.length > 0) {
     console.log(touches);
+    for (let i = 0; i < touches.length; i++) {
+      ellipse(touches[i].x, touches[i].y, 100, 100);
+    }
   }
-  for (let i = 0; i < touches.length; i++) {
-    ellipse(touches[i].x, touches[i].y, 100, 100);
-  }
+
   if (touches.length == 2) {
     line(touches[0].x, touches[0].y, touches[1].x, touches[1].y);
   }
