3aab44c502d8590f8a76fa0c560ed9130f6b8536
sahandbabali <sahand.babali@gmail.com>
2022-04-09 18:34:43 +0430

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(200);
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
