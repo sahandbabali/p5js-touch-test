93fef0b7d6aac64a666300847c3c7b3470a901c9
sahandbabali <sahand.babali@gmail.com>
2022-04-09 23:39:52 +0430

    first point added

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+var fontsize = 300;
+var fontcolor;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
@@ -6,17 +9,33 @@ function draw() {
   background(200);
   // clear();
   let display = touches.length + " touches";
+
   if (touches.length > 0) {
     console.log(touches);
-    for (let i = 0; i < touches.length - 1; i++) {
-      ellipse(touches[i].x, touches[i].y, 100, 100);
+    for (let i = 0; i < touches.length; i++) {
+      ellipse(touches[i].x, touches[i].y, 200, 200);
     }
   }
 
   if (touches.length == 2) {
     line(touches[0].x, touches[0].y, touches[1].x, touches[1].y);
   }
-  text(display, 5, 10);
+  textSize(16);
+  textAlign(CENTER);
+
+  text(display, windowWidth / 2, 20);
+
+  drawtheletter();
+}
+
+function drawtheletter() {
+  if (touches.length > 0) {
+    fontsize = map(touches[0].y, windowHeight / 2, windowHeight, 100, 500);
+  }
+
+  textSize(fontsize);
+  textAlign(CENTER);
+  text("ق", windowWidth / 2, windowHeight / 3);
 }
 
 function windowResized() {
